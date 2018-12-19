a114a4029142105b9b127a8bace4fdf9fac8f944
Jorge Gabriel Azevedo <jorge.gfm.azevedo@gmail.com>
2018-12-19 19:14:01 +0000

    BoxApp: Página Sobre

--- BoxApp/src/main/java/UI/AboutAppUI.java
+++ BoxApp/src/main/java/UI/AboutAppUI.java
@@ -9,6 +9,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
 /**
@@ -19,14 +20,15 @@ import javafx.scene.layout.AnchorPane;
 public class AboutAppUI implements Initializable {
 
     @FXML
-    private AnchorPane AnchorPane;
+    private Label lable1;
+
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        lable1.setText("Esta aplicaçao e desenvolvida pelo grupo de alunos Microwells e destina-se exclusivamente para avaliaçao na disciplina Laboratorios de Sistemas II lecionada no ISEP durante o 1º Smeestre do ano letivo 2018/2019.\nApenas este uso é premitido para esta versão da aplicação.");
     }    
     
 }
