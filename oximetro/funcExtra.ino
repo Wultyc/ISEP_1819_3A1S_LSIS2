853f5c165aea3072806dc0f56187d9003d724d0b
Brenda <1150744@isep.ipp.pt>
2018-12-20 17:02:45 +0000

    Oximetro: Funções extra

--- oximetro/funcExtra.ino
+++ oximetro/funcExtra.ino
@@ -1,15 +1,17 @@
 byte getCommand(){
-  
+  byte r = Serial.read();
+  Serial.print("T");
+  return r;
 }
 
 bool send_module_info(){
-  
+  Serial.print(MODULE_INFO + ";" + PROTOCOL_VER);
 }
 
 bool send_integrity_state(){
-  
+  Serial.print(T);  //Para já fica assim
 }
 
 bool send_nonDf(){
-  
+  Serial.print("E");
 }
