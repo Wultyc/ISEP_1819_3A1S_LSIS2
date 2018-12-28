e0b5ac2a7b058558a63df5ea1200ae45bcee4aeb
Jorge Gabriel Azevedo <jorge.gfm.azevedo@gmail.com>
2018-12-28 17:05:38 +0000

    BoxApp: Modelo da aplicação

--- BoxApp/src/main/java/Model/Medicao.java
+++ BoxApp/src/main/java/Model/Medicao.java
@@ -1,7 +1,44 @@
 
 package Model;
 
+import java.util.Date;
+
 
 public class Medicao {
+    private int id;
+    private Date timestamp;
+
+    public Medicao(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the timestamp
+     */
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * @param timestamp the timestamp to set
+     */
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
+    
     
 }
--- BoxApp/src/main/java/Model/MedicaoPulsoCardiaco.java
+++ BoxApp/src/main/java/Model/MedicaoPulsoCardiaco.java
@@ -1,7 +1,44 @@
 
 package Model;
 
+import java.util.Date;
+
 
 public class MedicaoPulsoCardiaco extends Medicao{
     
+    private int pulsoMedio;
+    
+    public MedicaoPulsoCardiaco(int id) {
+        super(id);
+    }
+
+    /**
+     * @return the pulsoMedio
+     */
+    public int getPulsoMedio() {
+        return pulsoMedio;
+    }
+
+    /**
+     * @param pulsoMedio the pulsoMedio to set
+     */
+    public void setPulsoMedio(int pulsoMedio) {
+        this.pulsoMedio = pulsoMedio;
+    }
+    
+    /**
+     * @return the timestamp
+     */
+    @Override
+    public Date getTimestamp() {
+        return super.getTimestamp();
+    }
+
+    /**
+     * @param timestamp the timestamp to set
+     */
+    @Override
+    public void setTimestamp(Date timestamp) {
+        super.setTimestamp(timestamp);
+    }
 }
